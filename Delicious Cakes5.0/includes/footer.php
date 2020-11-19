83f9cd22fec57032ad64310b43cb86ca44924e71
IngridPX <72896995+IngridPX@users.noreply.github.com>
2020-11-18 23:59:18 -0300

    Versao bootstrap

---
+++ Delicious Cakes5.0/includes/footer.php
@@ -0,0 +1,9 @@
+<!--Início do rodapé-->
+<footer>
+    <hr>
+    <h4><em>Formas de Pagamento:</em><br/>
+        <img src="imagens/forma_pagamento.jpg" title="Formas de Pagamento" alt='Formas de Pagamento'>
+        <p><strong>&copy;Todos os direitos reservados.</strong></p>
+    </h4>
+</footer>
+<!--Fim do rodapé -->
---
+++ Delicious Cakes5.0/includes/header.php
@@ -0,0 +1,13 @@
+<!-- Início do menu-->
+<header>
+    <nav class="navbar navbar-expand-sm bg-white">
+        <ul class="navbar-nav menu">
+            <li class="nav-item"><a href='Historia.php' onmouseover="this.className='trOver'" onmouseout="this.className='trOut'">A Delicious Cakes</a></li>
+            <li class="nav-item"><a href='Bolos.php' onmouseover="this.className='trOver'" onmouseout="this.className='trOut'">Bolos</a></li>
+            <li class="nav-item" id="logo"><a href='Home.php' onmouseover="this.className='trOver'" onmouseout="this.className='trOut'"><img src="imagens/Logo.png" alt="Delicious Cakes"></a></li>
+            <li class="nav-item"><a href='Lojas.php' onmouseover="this.className='trOver'" onmouseout="this.className='trOut'">Nossas Lojas</a></li>
+            <li class="nav-item"><a href='Contato.php' onmouseover="this.className='trOver'" onmouseout="this.className='trOut'">Contatos</a></li>
+        </ul>
+    </nav>
+</header>
+<!-- Fim do menu-->
