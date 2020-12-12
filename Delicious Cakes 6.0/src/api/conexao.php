d7b436fe6dbbb18d52ae88eaae755457f28e3e37
IngridPX <72896995+IngridPX@users.noreply.github.com>
2020-12-11 22:35:50 -0300

    Versao React com Bootstrap

---
+++ Delicious Cakes 6.0/src/api/conexao.php
@@ -0,0 +1,24 @@
+<?php
+function query($sql) {
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "dbDeliciousCakes";
+
+    $connect = mysqli_connect($servidor, $usuario, $senha, $banco);
+    $resultado = mysqli_query($connect, $sql);
+    mysqli_close($connect);
+    return $resultado;
+}
+    $result = query("SELECT produtos.nome, produtos.descricao, produtos.nome_imagem, precos.preco, precos.preco_desconto
+    FROM produtos JOIN precos ON produtos.id_prod = precos.id_preco;");
+    $produtos = [];
+    
+    while($row = mysqli_fetch_assoc($result)){
+        $produtos[] = $row;
+    }
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-type: application/json");
+    echo json_encode($produtos);
+?>
