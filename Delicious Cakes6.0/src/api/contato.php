2fb14ee7a35327a3ccc7b03159359062554c7d68
IngridPX <72896995+IngridPX@users.noreply.github.com>
2020-12-11 23:26:39 -0300

    Versao react-bootstrap

---
+++ Delicious Cakes6.0/src/api/contato.php
@@ -0,0 +1,21 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-type: application/json");
+    $link = mysqli_connect("localhost", "root","", "dbDeliciousCakes");
+    if (!$link)
+        die ("Falha na conexão com o BD " . mysqli_connect_error());
+    else
+        echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL.";
+        $categoria = ($_POST['categoria']);
+        $nome = ($_POST['Nome']);
+        $sobrenome = ($_POST['Sobrenome']);
+        $telefone = ($_POST['Telefone']);
+        $email = ($_POST['Email']);
+        $descricao = ($_POST['descricao']);
+    $insere = "Insert into recado ()
+    Values ('$categoria','$nome','$sobrenome','$telefone','$email','$descricao')";
+    mysqli_query ($link, $insere);
+    mysqli_close();
+
+    echo json_decode($insere);
+?>
